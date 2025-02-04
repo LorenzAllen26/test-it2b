fc92f63a689bde727ae218380207034a07faa263
Lorenz <lorenz_allen_biscocho@dlsl.edu.ph>
2025-02-04 09:21:37 +0800

    3rd Commit

--- testing1.cpp
+++ testing1.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int main()
 {
     std::cout<<"Hello World";
-
+    cout<<"Hello World";  
+    cout<<"Hello World";
+    cout<<"Hello World";
+    cout<<"Hello World";
+    cout<<"Hello World";  
+    cout<<"Hello World";
     return 0;
 }

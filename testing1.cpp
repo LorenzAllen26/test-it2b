bdcb307ee13e340fd575820d30e6d0681845c370
Lorenz <lorenz_allen_biscocho@dlsl.edu.ph>
2025-02-05 08:49:19 +0800

    My First Commit

--- testing1.cpp
+++ testing1.cpp
@@ -11,7 +11,7 @@ int main()
     cout<<"Hello World";  
     cout<<"Hello World";
 
-    cout<<"Hello World";
+    cout<<"Hello World";    
     cout<<"Hello World";
     cout<<"Hello World";
     cout<<"Hello World";
